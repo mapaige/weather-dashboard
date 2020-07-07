d842c851eec9ca8d9e85efb460e7e233f484d2f1
Marcia Paige <mapaige7@gmail.com>
2020-07-06 23:41:27 -0400

    Added minor changes

--- script.js
+++ script.js
@@ -13,6 +13,8 @@
  var index =document.querySelector(".index");
   var citySearch=document.querySelector("#city")
 var searchBtn=document.querySelector("#searchBtn")
+var groupEl=document.querySelector(".card-group")
+var day1=document.querySelector("#day1")
 
 searchBtn.addEventListener("click",function(){
   // api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
@@ -20,6 +22,8 @@ searchBtn.addEventListener("click",function(){
 
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citySearch.value}&units=imperial&appid=${"786dd1a7d0584cd0a7a388fa0171ce54"}`, {
   method: 'GET' 
+
+ 
 })
 .then(response => response.json())
 .then(result => {
@@ -27,20 +31,45 @@ searchBtn.addEventListener("click",function(){
     var citySpan=document.createElement("span")
     citySpan.textContent=result.name
     headerEl.appendChild(citySpan)
+     
 
     var dateSpan=document.createElement("span")
     dateSpan.textContent= " ("+ moment(result.dt,"X").format("MM/DD/YYYY") +")"
  headerEl.appendChild(dateSpan)
+ 
  var iconImg=document.createElement("img")
  
  var iconurl = "http://openweathermap.org/img/w/" + result.weather[0].icon + ".png";
 
  iconImg.setAttribute("src",iconurl)
  headerEl.appendChild(iconImg)
+
+ var descriptionSpan=document.createElement("span")
+    descriptionSpan.textContent=result.weather[0].description
+    condition.appendChild(descriptionSpan)
+
+    var tempSpan=document.createElement("span")
+    tempSpan.textContent=result.main.temp  
+
+    temp.appendChild(tempSpan)
+
+    var humiditySpan=document.createElement("span")
+    humiditySpan.textContent=result.main.humidity
+    humidity.appendChild(humiditySpan)
+
+    var windSpeedSpan=document.createElement("span")
+    windSpeedSpan.textContent=result.wind.speed
+    windSpeed.appendChild(windSpeedSpan)
+
+    
+
+  
+
 })
 .catch(error => {
   console.error('Error:', error);
 });
+
 
 
   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${citySearch.value}&appid=${"786dd1a7d0584cd0a7a388fa0171ce54"}`, {
